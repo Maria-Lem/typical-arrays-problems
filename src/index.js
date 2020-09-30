3e832a95d03dae0ce73e464af0d2dacc045088c3
Maria <maria131621@yandex.ru>
2020-09-30 13:25:36 +0300

    feat: add min, max and average functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    const minResult = Math.min(...array);
+    return minResult;
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    const forLoopMax = () => {
+      let max = array[0];
+  
+      for (let i = 1; i < array.length; i++) {
+        let value = array[i];
+        max = (value > max) ? value : max;
+      }
+      return [max];
+    }
+    const maxResult = forLoopMax();
+    return maxResult;
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let count = 0;
+    let total = 0;
+  
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] !== undefined) {
+        count++;
+        total += array[i];
+      }
+    }
+  
+    let avgArr = total / count;
+    return avgArr;
+  } else {
+    return 0;
+  }
 }
